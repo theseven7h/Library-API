5ebdf817f336536899c545ce2c9ad397b971fe1f
James Tauri <jamestauri123@gmail.com>
2025-08-17 15:17:42 +0100

    Delete library/src/main/java/com/library directory
    initial library file deleted after creating a new one

--- library/src/main/java/com/library/data/repository/Library.java
+++
@@ -1,9 +0,0 @@
-package com.library.data.repository;
-
-import com.library.data.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Library extends JpaRepository<Book, Long> {
-}
